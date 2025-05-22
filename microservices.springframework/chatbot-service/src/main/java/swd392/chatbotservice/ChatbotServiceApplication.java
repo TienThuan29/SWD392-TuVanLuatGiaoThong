bf1157b166a8bcbcab8b804291b61faf3a9158a5
TienThuan29 <nguyentienthuan9595@gmail.com>
2025-05-22 11:26:37 +0700

    update setup be

--- microservices.springframework/chatbot-service/src/main/java/swd392/chatbotservice/ChatbotServiceApplication.java
+++ microservices.springframework/chatbot-service/src/main/java/swd392/chatbotservice/ChatbotServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatboxServiceApplication {
+public class ChatbotServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatboxServiceApplication.class, args);
+		SpringApplication.run(ChatbotServiceApplication.class, args);
 	}
 
 }
